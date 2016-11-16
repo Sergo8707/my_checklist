78736c876ccd047e6578232cf27c6e5c7f9ec4ac
Sergo <sergo8707@gmail.com>
2016-11-16 13:33:44 +0200

    vendor compiled assets 3

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,9 @@ Bundler.require(*Rails.groups)
 module Checklist
   class Application < Rails::Application
 
-    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+    # Enable the asset pipeline
+    config.assets.enabled = true
+    config.assets.paths << "#{Rails.root}/app/assets/fonts" 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
